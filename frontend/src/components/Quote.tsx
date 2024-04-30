80455e9003b72047e8d0eabfd6b372ec93226c2a
aashish_k <aashishkathat00@gmail.com>
2024-04-30 22:30:38 +0530

    changed quote

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,17 +1,13 @@
 function Quote() {
 	return (
-		<div className='bg-slate-200 h-screen flex justify-center flex-col'>
+		<div className='flex flex-col justify-center h-screen bg-slate-200'>
 			<div className='flex justify-center'>
 				<div className='max-w-lg '>
 					<div className='text-3xl font-bold'>
-						"The customer support I received was execeptional. The
-						support team went above and beyond to address my
-						concerns."
+						"This the best blog app I have ever witnessed."
 					</div>
 
-					<div className='max-w-md text-xl font-semibold mt-4 '>
-						Mr Robert kumar
-					</div>
+					<div className='max-w-md mt-4 text-xl font-semibold '>Mr Robert kumar</div>
 					<div className='max-w-md text-sm font-light text-slate-400'>
 						CEO | digital river
 					</div>
